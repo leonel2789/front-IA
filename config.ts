1929fdac876cf26e72917ed4cae7b63a34e8ccfd
leonel2789 <leofer-acosta.755@hotmail.com>
2025-08-13 19:18:26 -0300

    a

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const N8N_URL = process.env.EXPO_PUBLIC_N8N_URL || 
-  'https://legalbackn8n.nilosolutions.com/webhook-test/66c6b4ae-eae4-411c-ad4f-64a359ec245f';
+  'https://legalbackn8n.nilosolutions.com/webhook/66c6b4ae-eae4-411c-ad4f-64a359ec245f';
 
 export interface ChatMessage {
   message: string;
